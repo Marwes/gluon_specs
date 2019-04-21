26f025568888bc954e147f00257db29ae0ca263c
Markus Westerlind <marwes91@gmail.com>
2019-04-21 16:03:13 +0200

    doc(example): Draw a sprite

--- examples/asteroids/asteroids.rs
+++ examples/asteroids/asteroids.rs
@@ -1,34 +1,54 @@
 use amethyst::{
+    assets::{AssetStorage, Loader},
     core::transform::Transform,
     prelude::*,
-    renderer::{Camera, Event, KeyboardInput, Projection, VirtualKeyCode, WindowEvent},
+    renderer::{
+        Camera, Event, KeyboardInput, PngFormat, Projection, SpriteRender, SpriteSheet,
+        SpriteSheetFormat, Texture, TextureMetadata, VirtualKeyCode, WindowEvent,
+    },
 };
 
-pub const ARENA_HEIGHT: f32 = 100.0;
-pub const ARENA_WIDTH: f32 = 100.0;
+use gluon_codegen::{Getable, Pushable, VmType};
+use specs_derive::Component;
 
-fn initialise_camera(world: &mut World) {
-    let mut transform = Transform::default();
-    transform.set_z(1.0);
-    world
-        .create_entity()
-        .with(Camera::from(Projection::orthographic(
-            0.0,
-            ARENA_WIDTH,
-            0.0,
-            ARENA_HEIGHT,
-        )))
-        .with(transform)
-        .build();
+pub const ARENA_HEIGHT: f32 = 300.0;
+pub const ARENA_WIDTH: f32 = 300.0;
+
+#[derive(Component, VmType, Getable, Pushable)]
+#[gluon(newtype)]
+struct Player;
+
+#[derive(Clone, Debug, Component, VmType, Getable, Pushable)]
+#[gluon(newtype)]
+struct Position {
+    x: f64,
+    y: f64,
+}
+gluon_specs::impl_clone_marshal!(Position);
+
+pub struct Asteroids {
+    thread: gluon::RootedThread,
 }
 
-pub struct Asteroids;
+impl Asteroids {
+    pub fn new() -> Self {
+        Asteroids {
+            thread: gluon::new_vm(),
+        }
+    }
+}
 
 impl SimpleState for Asteroids {
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
         let world = data.world;
 
+        gluon_specs::init_resources(&mut world.res, &self.thread);
+
+        world.register::<Player>();
+        gluon_specs::register_component::<Position>(world, &self.thread, "Position");
+
         initialise_camera(world);
+        initialise_player(world);
     }
 
     fn handle_event(&mut self, _: StateData<GameData>, event: StateEvent) -> SimpleTrans {
@@ -56,4 +76,58 @@ impl SimpleState for Asteroids {
     fn update(&mut self, _: &mut StateData<GameData>) -> SimpleTrans {
         Trans::None
     }
+}
+
+fn initialise_camera(world: &mut World) {
+    let mut transform = Transform::default();
+    transform.set_z(1.0);
+    world
+        .create_entity()
+        .with(Camera::from(Projection::orthographic(
+            0.0,
+            ARENA_WIDTH,
+            0.0,
+            ARENA_HEIGHT,
+        )))
+        .with(transform)
+        .build();
+}
+
+fn initialise_player(world: &mut World) {
+    let texture_handle = {
+        let loader = world.read_resource::<Loader>();
+        let texture_storage = world.read_resource::<AssetStorage<Texture>>();
+        loader.load(
+            "texture/asteroids_spritesheet.png",
+            PngFormat,
+            TextureMetadata::srgb_scale(),
+            (),
+            &texture_storage,
+        )
+    };
+
+    let sprite_sheet = {
+        let loader = world.read_resource::<Loader>();
+        let sprite_sheet_store = world.read_resource::<AssetStorage<SpriteSheet>>();
+        loader.load(
+            "texture/asteroids_spritesheet.ron", // Here we load the associated ron file
+            SpriteSheetFormat,
+            texture_handle, // We pass it the handle of the texture we want it to use
+            (),
+            &sprite_sheet_store,
+        )
+    };
+
+    let mut transform = Transform::default();
+    transform.set_x(ARENA_WIDTH / 2.).set_y(ARENA_HEIGHT / 2.);
+
+    world
+        .create_entity()
+        .with(SpriteRender {
+            sprite_sheet,
+            sprite_number: 0,
+        })
+        .with(transform)
+        .with(Player)
+        .build();
 }
--- examples/asteroids/main.rs
+++ examples/asteroids/main.rs
@@ -3,14 +3,17 @@ extern crate amethyst;
 use amethyst::{
     core::transform::TransformBundle,
     prelude::*,
-    renderer::{DisplayConfig, DrawFlat, Pipeline, PosNormTex, RenderBundle, Stage},
+    renderer::{DisplayConfig, DrawFlat2D, Pipeline, RenderBundle, Stage},
     utils::application_root_dir,
 };
 
 mod asteroids;
 
 fn main() -> amethyst::Result<()> {
-    amethyst::start_logger(Default::default());
+    amethyst::start_logger(amethyst::LoggerConfig {
+        level_filter: amethyst::LogLevelFilter::Warn,
+        ..Default::default()
+    });
 
     let path = format!(
         "{}/examples/asteroids/resources/display_config.ron",
@@ -21,15 +24,15 @@ fn main() -> amethyst::Result<()> {
     let pipe = Pipeline::build().with_stage(
         Stage::with_backbuffer()
             .clear_target([0.0, 0.0, 0.0, 1.0], 1.0)
-            .with_pass(DrawFlat::<PosNormTex>::new()),
+            .with_pass(DrawFlat2D::new()),
     );
 
-    let state = asteroids::Asteroids;
+    let state = asteroids::Asteroids::new();
 
     let game_data = GameDataBuilder::default()
-        .with_bundle(RenderBundle::new(pipe, Some(config)))?
+        .with_bundle(RenderBundle::new(pipe, Some(config)).with_sprite_sheet_processor())?
         .with_bundle(TransformBundle::new())?;
-    let mut game = Application::new("./", state, game_data)?;
+    let mut game = Application::new("./examples/assets", state, game_data)?;
 
     game.run();
 
